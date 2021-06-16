57a613c0ffc202ab13be1a82b4abf89a93f9271d
soulsam480 <soulsam480@hotmail.com>
2021-06-16 22:05:05 +0530

    feat(api): add user data endpoint

--- api/src/auth/index.ts
+++ api/src/auth/index.ts
@@ -8,31 +8,20 @@ import { RequestWithUser } from 'src/utils/types';
 
 export const authRouter = Router();
 
-authRouter.get(
-  '/google',
-  authenticate('google', { scope: ['profile', 'email'] }),
-);
+authRouter.get('/google', authenticate('google', { scope: ['profile', 'email'] }));
 
-authRouter.get(
-  '/google/redirect',
-  authenticate('google'),
-  (req: RequestWithUser, res) => {
-    const uid = req?.user?.id;
-    const token = sign(
-      { userId: uid },
-      process.env.ACCESS_TOKEN_SECRET as string,
-      {
-        expiresIn: '15min',
-      },
-    );
+authRouter.get('/google/redirect', authenticate('google'), (req: RequestWithUser, res) => {
+  const uid = req?.user?.id;
+  const token = sign({ userId: uid }, process.env.ACCESS_TOKEN_SECRET as string, {
+    expiresIn: '15min',
+  });
 
-    const redirectUrl = !process.env.PROD
-      ? `http://localhost:4000/?auth_success=${token}`
-      : `https://furikaeru.sambitsahoo.com/?auth_success=${token}`;
+  const redirectUrl = !process.env.PROD
+    ? `http://localhost:4000/#/?auth_success=${token}`
+    : `https://furikaeru.sambitsahoo.com/?auth_success=${token}`;
 
-    res.redirect(redirectUrl);
-  },
-);
+  res.redirect(redirectUrl);
+});
 
 authRouter.get('/token', async (request, response) => {
   const refreshToken = request.headers['refresh-token'] as string;
@@ -42,13 +31,34 @@ authRouter.get('/token', async (request, response) => {
     return response.status(400).send(ERROR_MESSAGES.refresh_token_not_found);
 
   try {
-    const data = <{ userId: string }>(
-      verify(token, process.env.REFRESH_TOKEN_SECRET as string)
-    );
+    const data = <{ userId: string }>verify(token, process.env.REFRESH_TOKEN_SECRET as string);
 
     const user = await User.findOne({ id: data.userId });
-    response.send(createTokens(user as User));
+
+    if (!user) return response.status(404).send(ERROR_MESSAGES.user_not_found);
+
+    response.send(createTokens(user));
   } catch {
     response.status(400).send(ERROR_MESSAGES.acccess_token_expired);
+  }
+});
+
+authRouter.get('/user', async (req, res) => {
+  const accessToken = req.headers['authorization'] as string;
+
+  const token = accessToken.split('Bearer ')[1];
+
+  if (typeof accessToken !== 'string' || !token) return res.status(401).send(ERROR_MESSAGES.unauthorized);
+
+  try {
+    const data = <{ userId: string }>verify(token, process.env.ACCESS_TOKEN_SECRET as string);
+
+    const user = await User.findOne({ id: data.userId });
+
+    if (!user) return res.status(404).send(ERROR_MESSAGES.user_not_found);
+
+    res.send({ ...user, ...createTokens(user) });
+  } catch {
+    res.status(400).send(ERROR_MESSAGES.acccess_token_expired);
   }
 });
