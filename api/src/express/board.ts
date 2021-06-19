d5d862f9e5d185ee2aa42b2fffc58e7967459f18
soulsam480 <soulsam480@hotmail.com>
2021-06-19 08:26:16 +0530

    feat(api): board crud routes

--- api/src/express/board.ts
+++ api/src/express/board.ts
@@ -1,13 +1,88 @@
 import { Router } from 'express';
 import { Board } from 'src/entities/board';
 import { authMiddleware } from 'src/middlewares/authMiddleware';
+import { ERROR_MESSAGES } from 'src/utils/constants';
 import { RequestWithUser } from 'src/utils/types';
 
 const boardRouter = Router();
 
 boardRouter.post('/', authMiddleware, async (req: RequestWithUser, res) => {
   const { userId, body } = req;
-  const createdBoard = await Board.create({ user: { id: userId }, data: body.data, is_public: body.is_public }).save();
-  res.send(createdBoard);
+  try {
+    const createdBoard = await Board.create({
+      user: { id: userId },
+      data: body.data,
+      is_public: body.is_public,
+    }).save();
+
+    res.send(createdBoard);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send(ERROR_MESSAGES.iss);
+  }
 });
+
+boardRouter.get('/', authMiddleware, async (req: RequestWithUser, res) => {
+  const { userId } = req;
+  try {
+    const userBoards = await Board.find({ where: { user: { id: userId } } });
+
+    res.send(userBoards);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send(ERROR_MESSAGES.iss);
+  }
+});
+
+boardRouter.get('/:id', authMiddleware, async (req: RequestWithUser, res) => {
+  const {
+    userId,
+    params: { id },
+  } = req;
+  try {
+    const userBoard = await Board.findOne({ where: { id, user: { id: userId } } });
+    if (!userBoard) return res.status(404).send(ERROR_MESSAGES.not_found);
+
+    res.send(userBoard);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send(ERROR_MESSAGES.iss);
+  }
+});
+
+boardRouter.patch('/:id', authMiddleware, async (req: RequestWithUser, res) => {
+  const {
+    userId,
+    params: { id },
+  } = req;
+  try {
+    const body = req.body as Partial<Board>;
+
+    await Board.update({ id, user: { id: userId } }, { ...body });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send(ERROR_MESSAGES.iss);
+  }
+});
+
+boardRouter.delete('/:id', authMiddleware, async (req: RequestWithUser, res) => {
+  const {
+    userId,
+    params: { id },
+  } = req;
+  try {
+    await Board.delete({ id, user: { id: userId } });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send(ERROR_MESSAGES.iss);
+  }
+});
+
 export { boardRouter };
--- api/src/utils/constants.ts
+++ api/src/utils/constants.ts
@@ -8,6 +8,7 @@ export const ERROR_MESSAGES = {
   email_in_use: 'Email already in use !',
   iss: 'Something went wrong !',
   user_exists_with_credentials: 'User with same email or username exists !',
+  not_found: 'Not found !',
 };
 
 export const CORS_ORIGINS = ['http://localhost:4000', 'http://localhost:5000'];
