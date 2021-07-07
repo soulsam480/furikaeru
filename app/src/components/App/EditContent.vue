f87eb58bf09c8d62e146dd23234b935d2a54613c
soulsam480 <soulsam480@hotmail.com>
2021-07-07 21:08:54 +0530

    feat(app): Escape to dismiss editcontent

--- app/src/components/App/EditContent.vue
+++ app/src/components/App/EditContent.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
-import { defineEmit, defineProps, ref } from 'vue';
+import { defineEmit, defineProps, onBeforeUnmount, onMounted, ref } from 'vue';
 import FButton from 'src/components/lib/FButton.vue';
+
 const props = defineProps<{
   content: string;
   color?: string;
 }>();
-
 const emit = defineEmit({
   save: String,
   cancel: null,
 });
+
 const newVal = ref(props.content);
 
 function handleSave() {
   if (newVal.value === props.content) return;
   emit('save', newVal.value);
 }
+
+function handleClose(e: KeyboardEvent) {
+  const { key } = e;
+  if (key === 'Escape') return emit('cancel');
+}
+
+onMounted(() => window.addEventListener('keydown', handleClose));
+onBeforeUnmount(() => window.removeEventListener('keydown', handleClose));
 </script>
 <template>
   <input
