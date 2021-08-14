ab0406c64d291ca074892864aa16f37433128832
soulsam480 <soulsam480@hotmail.com>
2021-08-14 20:26:36 +0530

    chore(app): update deps

---
+++ app/sync.js
@@ -0,0 +1,31 @@
+// this is a small script to install latest deps
+// after running this, don't forget to install vue@next@latest vue-router@next@latest pinia@next@latest
+
+const execSync = require('child_process').execSync;
+
+const { dependencies, devDependencies, syncIgnore } = require('./package.json');
+
+Object.keys(dependencies)
+  .filter((el) => !syncIgnore.includes(el))
+  .forEach((key) => {
+    try {
+      execSync(`yarn remove ${key}`, { encoding: 'utf-8' });
+      execSync(`yarn add ${key}`, { encoding: 'utf-8' });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+Object.keys(devDependencies).forEach((key) => {
+  try {
+    execSync(`yarn remove ${key}`, { encoding: 'utf-8' });
+    execSync(`yarn add ${key} -D`, { encoding: 'utf-8' });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+syncIgnore.forEach((el) => {
+  execSync(`yarn remove ${el}`, { encoding: 'utf-8' });
+  execSync(`yarn add ${el}@next`, { encoding: 'utf-8' });
+});
